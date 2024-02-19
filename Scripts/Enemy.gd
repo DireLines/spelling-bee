155f518a59cf6279c2445aec3e7d894c4820d051
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2024-02-19 17:55:21 -0500

    send bullet back if wrong letter

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -4,6 +4,11 @@ extends CharacterBody2D
 
 var killphrase = ""
 var hit_letters = []
+	
+func _ready():
+	area_2d.connect("body_entered", Callable(self, "_on_body_entered"))
+	set_killphrase("dog")
+	#area_2d.connect("body_exited", Callable(self, "_on_body_exited"))
 
 func set_killphrase(phrase):
 	killphrase = phrase.to_upper()
@@ -11,12 +16,27 @@ func set_killphrase(phrase):
 	for letter in killphrase:
 		hit_letters.append(false)
 	get_node("Word").text = killphrase
-	
-func _ready():
-	area_2d.connect("body_entered", Callable(self, "_on_body_entered"))
-	set_killphrase("dog")
-	#area_2d.connect("body_exited", Callable(self, "_on_body_exited"))
 
+func _on_body_entered(body):
+	var label = body.get_node_or_null("Letter")
+	if label != null:
+		var letter = label.text
+		var letter_index = get_next_hittable_letter_index()
+		if letter_index == -1:
+			print("enemy already dead")
+			return # all letters hit - enemy already dead
+		var needed_letter = killphrase[letter_index]
+		if letter == needed_letter:
+			hit_letter(letter_index)
+			body.queue_free()
+		else:
+			#TODO move body to enemy bullet layer
+			body.linear_velocity *= -0.8
+		
+#
+#func _on_body_exited(body):
+	#pass
+	
 func get_next_hittable_letter_index():
 	for i in range(len(killphrase)):
 		if !hit_letters[i]:
@@ -35,24 +55,3 @@ func hit_letter(index):
 			return # not all letters hit yet
 	#TODO play death sfx and stuff
 	queue_free()
-	
-
-func _on_body_entered(body):
-	var label = body.get_node_or_null("Letter")
-	if label != null:
-		var letter = label.text
-		var letter_index = get_next_hittable_letter_index()
-		if letter_index == -1:
-			print("enemy already dead")
-			return # all letters hit - enemy already dead
-		var needed_letter = killphrase[letter_index]
-		if letter == needed_letter:
-			hit_letter(letter_index)
-			print("right letter")
-		else:
-			print("wrong letter")
-			#TODO throw bullet back at player
-		
-#
-#func _on_body_exited(body):
-	#pass
