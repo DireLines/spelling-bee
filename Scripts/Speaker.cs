605899bf7458a151f16766eb80d6ef9478a96150
SimonMarkWhittle <simonmarkwhittle@gmail.com>
2024-02-19 22:25:56 -0500

    Get a windows computer loser

--- Scripts/Speaker.cs
+++ Scripts/Speaker.cs
@@ -1,37 +1,44 @@
 using Godot;
+using System;
 using System.Speech.Synthesis;
 
 public partial class Speaker : Node
 {
 	public static AudioStreamWav GetKillphraseAudio(string killphrase) {
-		string wordpath = ProjectSettings.GlobalizePath("res://Audio/Speech/" + killphrase + ".wav");
-		AudioStreamWav sound = new AudioStreamWav();
-		if (!System.IO.File.Exists(wordpath)) {
-            SpeechSynthesizer ss = new SpeechSynthesizer
-            {
-                Volume = 100
-            };
-            ss.SelectVoiceByHints(VoiceGender.Male, VoiceAge.Adult);
+        AudioStreamWav sound = new()
+        {
+            Format = AudioStreamWav.FormatEnum.Format16Bits,
+            MixRate = 22050
+        };
 
-			var audioStream = new System.IO.MemoryStream();
-			var formatInfo = new System.Speech.AudioFormat.SpeechAudioFormatInfo(
-				22050, System.Speech.AudioFormat.AudioBitsPerSample.Sixteen, System.Speech.AudioFormat.AudioChannel.Mono
-			);
+        try
+        {
+			string wordpath = ProjectSettings.GlobalizePath("res://Audio/Speech/" + killphrase + ".wav");
+			if (!System.IO.File.Exists(wordpath)) {
+				SpeechSynthesizer ss = new()
+				{
+					Volume = 100
+				};
+				ss.SelectVoiceByHints(VoiceGender.Male, VoiceAge.Adult);
 
-			ss.SetOutputToAudioStream(audioStream, formatInfo);
-			ss.Speak(killphrase);
-			ss.SetOutputToNull();
+				var audioStream = new System.IO.MemoryStream();
+				var formatInfo = new System.Speech.AudioFormat.SpeechAudioFormatInfo(
+					22050, System.Speech.AudioFormat.AudioBitsPerSample.Sixteen, System.Speech.AudioFormat.AudioChannel.Mono
+				);
 
-			byte[] buffer = audioStream.ToArray();
+				ss.SetOutputToAudioStream(audioStream, formatInfo);
+				ss.Speak(killphrase);
+				ss.SetOutputToNull();
 
-			sound.Data = buffer;
-			sound.Format = AudioStreamWav.FormatEnum.Format16Bits;
-			sound.MixRate = 22050;
-			sound.SaveToWav(wordpath);
-		} else {
-			sound = ResourceLoader.Load<AudioStreamWav>(wordpath);
+				byte[] buffer = audioStream.ToArray();
+				sound.Data = buffer;
+				sound.SaveToWav(wordpath);
+			} else {
+				sound = ResourceLoader.Load<AudioStreamWav>(wordpath);
+			}
+		} catch (PlatformNotSupportedException _) {
+			GD.Print("Platform not supported for Speech Synthesis - get a windows computer, loser.");
 		}
-
 		return sound;
 	}
 }
